ed0710e85787e594c35886206767e54d00b377f5
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2017-02-26 20:31:33 +0000

    Eibe's fixes relating to predictions made by the InputMappedClassifier
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/stable-3-8@14042 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/knowledgeflow/steps/PredictionAppender.java
+++ weka/src/main/java/weka/knowledgeflow/steps/PredictionAppender.java
@@ -22,12 +22,9 @@
 package weka.knowledgeflow.steps;
 
 import weka.classifiers.UpdateableClassifier;
+import weka.classifiers.misc.InputMappedClassifier;
 import weka.clusterers.DensityBasedClusterer;
-import weka.core.DenseInstance;
-import weka.core.Instance;
-import weka.core.Instances;
-import weka.core.OptionMetadata;
-import weka.core.WekaException;
+import weka.core.*;
 import weka.filters.unsupervised.attribute.Add;
 import weka.gui.knowledgeflow.KFGUIConsts;
 import weka.knowledgeflow.Data;
@@ -596,6 +593,12 @@ public class PredictionAppender extends BaseStep {
     weka.classifiers.Classifier classifier, boolean distribution,
     String relationNameModifier) throws Exception {
 
+    // get class attribute from InputMappedClassifier (if necessary)
+    Attribute classAttribute = inputData.classAttribute();
+    if (classifier instanceof weka.classifiers.misc.InputMappedClassifier) {
+      classAttribute = ((InputMappedClassifier) classifier).getModelHeader(new Instances(inputData, 0)).classAttribute();
+    }
+
     String classifierName = classifier.getClass().getName();
     classifierName =
       classifierName.substring(classifierName.lastIndexOf('.') + 1,
@@ -603,11 +606,11 @@ public class PredictionAppender extends BaseStep {
     Instances newData = new Instances(inputData);
 
     if (distribution) {
-      for (int i = 0; i < inputData.classAttribute().numValues(); i++) {
+      for (int i = 0; i < classAttribute.numValues(); i++) {
         Add addF = new Add();
         addF.setAttributeIndex("last");
         addF.setAttributeName(classifierName + "_prob_"
-          + inputData.classAttribute().value(i));
+          + classAttribute.value(i));
         addF.setInputFormat(newData);
         newData = weka.filters.Filter.useFilter(newData, addF);
       }
@@ -615,10 +618,10 @@ public class PredictionAppender extends BaseStep {
       Add addF = new Add();
       addF.setAttributeIndex("last");
       addF.setAttributeName("class_predicted_by: " + classifierName);
-      if (inputData.classAttribute().isNominal()) {
-        String classLabels = inputData.classAttribute().value(0);
-        for (int i = 1; i < inputData.classAttribute().numValues(); i++) {
-          classLabels += "," + inputData.classAttribute().value(i);
+      if (classAttribute.isNominal()) {
+        String classLabels = classAttribute.value(0);
+        for (int i = 1; i < classAttribute.numValues(); i++) {
+          classLabels += "," + classAttribute.value(i);
         }
         addF.setNominalLabels(classLabels);
       }
