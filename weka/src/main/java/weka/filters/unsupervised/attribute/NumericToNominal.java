9ac9a7b444b5dd0401d4b1d27bae0ece1ce8bd2c
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2017-05-31 23:28:53 +0000

    now skipping duplicate labels due to cut off of decimals using Utils.doubleToString(double,MAX_DECIMALS), e.g.: 2.00000001 and 2.0 now get merged into the same label
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/stable-3-8@14274 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/filters/unsupervised/attribute/NumericToNominal.java
+++ weka/src/main/java/weka/filters/unsupervised/attribute/NumericToNominal.java
@@ -20,12 +20,6 @@
 
 package weka.filters.unsupervised.attribute;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.Vector;
-
 import weka.core.Attribute;
 import weka.core.Capabilities;
 import weka.core.Capabilities.Capability;
@@ -38,6 +32,12 @@ import weka.core.RevisionUtils;
 import weka.core.SparseInstance;
 import weka.core.Utils;
 import weka.filters.SimpleBatchFilter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Vector;
 
 /**
  * <!-- globalinfo-start --> A filter for turning numeric attributes into
@@ -351,7 +351,9 @@ public class NumericToNominal extends SimpleBatchFilter {
         if (isDate) {
           values.add(data.attribute(i).formatDate(o.doubleValue()));
         } else {
-          values.add(Utils.doubleToString(o.doubleValue(), MAX_DECIMALS));
+          String label = Utils.doubleToString(o.doubleValue(), MAX_DECIMALS);
+          if (!values.contains(label))
+            values.add(label);
         }
       }
       Attribute newAtt = new Attribute(data.attribute(i).name(), values);
